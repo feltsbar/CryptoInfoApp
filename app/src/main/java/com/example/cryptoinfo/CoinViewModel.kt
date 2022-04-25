2260e4242e7ad8e1cb5a16c685f341fba9119927
feltsbar <kuzmin.max6569@gmail.com>
2022-04-25 23:41:52 +0300

    Created methods loadData() and getPriceListFromRawData() in CoinViewModel

---
+++ app/src/main/java/com/example/cryptoinfo/CoinViewModel.kt
@@ -0,0 +1,56 @@
+package com.example.cryptoinfo
+
+import android.app.Application
+import android.util.Log
+import androidx.lifecycle.AndroidViewModel
+import com.example.cryptoinfo.api.ApiFactory
+import com.example.cryptoinfo.database.AppDatabase
+import com.example.cryptoinfo.pojo.CoinPriceInfo
+import com.example.cryptoinfo.pojo.CoinPriceInfoRawData
+import com.google.gson.Gson
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
+
+class CoinViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val db = AppDatabase.getInstance(application)
+    private val compositeDisposable = CompositeDisposable()
+    val priceList = db.coinPriceInfoDao().getPriceList()
+
+    fun loadData() {
+        val disposable = ApiFactory.apiService.getTopCoinsInfo()
+            .map { it.data?.map { it.coinInfo?.name }?.joinToString(",") }
+            .flatMap { ApiFactory.apiService.getFullPriceList(fSyms = it) }
+            .map { getPriceListFromRawData(it) }
+            .subscribeOn(Schedulers.io())
+            .subscribe({
+                db.coinPriceInfoDao().insertPriceList(it)
+                Log.d("TEST_OF_LOADING_DATA", "Success: $it")
+            }, {
+                Log.d("TEST_OF_LOADING_DATA", "Failure: ${it.message}")
+            })
+    }
+
+    private fun getPriceListFromRawData(coinPriceInfoRawData: CoinPriceInfoRawData): List<CoinPriceInfo> {
+        val result = ArrayList<CoinPriceInfo>()
+        val jsonObject = coinPriceInfoRawData.coinPriceInfoJsonObject ?: return result
+        val coinKeySet = jsonObject.keySet()
+        for (coinKey in coinKeySet) {
+            val currencyJson = jsonObject.getAsJsonObject(coinKey)
+            val currencyKeySet = currencyJson.keySet()
+            for (currencyKey in currencyKeySet) {
+                val priceInfo = Gson().fromJson(
+                    currencyJson.getAsJsonObject(currencyKey),
+                    CoinPriceInfo::class.java
+                )
+                result.add(priceInfo)
+            }
+        }
+        return result
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        compositeDisposable.dispose()
+    }
+}
--- app/src/main/java/com/example/cryptoinfo/MainActivity.kt
+++ app/src/main/java/com/example/cryptoinfo/MainActivity.kt
@@ -3,33 +3,21 @@ package com.example.cryptoinfo
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import com.example.cryptoinfo.api.ApiFactory
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.disposables.CompositeDisposable
-import io.reactivex.schedulers.Schedulers
-import kotlinx.android.synthetic.main.activity_main.*
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : AppCompatActivity() {
-
-    private val compositeDisposable = CompositeDisposable()
+    private lateinit var viewModel: CoinViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val disposable = ApiFactory.apiService.getTopCoinsInfo()
-            .subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe({
-                Log.d("TEST_OF_LOADING_DATA", it.toString())
-            }, {
-                Log.d("TEST_OF_LOADING_DATA", it.message.toString())
-            })
-        compositeDisposable.add(disposable)
+        viewModel = ViewModelProvider(this)[CoinViewModel::class.java]
+        viewModel.loadData()
+        viewModel.priceList.observe(this, Observer {
+            Log.d("TEST_OF_LOADING_DATA", "Success im Activity: $it")
+        })
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        compositeDisposable.dispose()
-    }
 }
