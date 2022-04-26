0158a27be9acb7ae7bcef9edbcb36e6bce017f7b
feltsbar <kuzmin.max6569@gmail.com>
2022-04-26 14:41:04 +0300

    Added CoinInfoAdapter for RecyclerView

--- app/src/main/java/com/example/cryptoinfo/CoinPriceListActivity.kt
+++ app/src/main/java/com/example/cryptoinfo/CoinPriceListActivity.kt
@@ -5,6 +5,9 @@ import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.example.cryptoinfo.adapters.CoinInfoAdapter
+import com.example.cryptoinfo.pojo.CoinPriceInfo
+import kotlinx.android.synthetic.main.activity_coin_price_list.*
 
 class CoinPriceListActivity : AppCompatActivity() {
     private lateinit var viewModel: CoinViewModel
@@ -13,12 +16,17 @@ class CoinPriceListActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_coin_price_list)
 
+        val adapter = CoinInfoAdapter(this)
+        rvCoinPriceList.adapter = adapter
+        adapter.onCoinClickListener = object : CoinInfoAdapter.OnCoinClickListener{
+            override fun onCoinClick(coinPriceInfo: CoinPriceInfo) {
+                Log.d("ON_CLICK_TEST", coinPriceInfo.fromSymbol)
+            }
+
+        }
         viewModel = ViewModelProvider(this)[CoinViewModel::class.java]
-//        viewModel.priceList.observe(this, Observer {
-//            Log.d("TEST_OF_LOADING_DATA", "Success im Activity: $it")
-//        })
-        viewModel.getDetailInfo("BTC").observe(this, Observer {
-            Log.d("TEST_OF_LOADING_DATA", "Success im Activity: $it")
+        viewModel.priceList.observe(this, Observer {
+            adapter.coinInfoList = it
         })
     }
 
---
+++ app/src/main/java/com/example/cryptoinfo/adapters/CoinInfoAdapter.kt
@@ -0,0 +1,55 @@
+package com.example.cryptoinfo.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.cryptoinfo.R
+import com.example.cryptoinfo.pojo.CoinPriceInfo
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.item_coin_info.view.*
+
+class CoinInfoAdapter(private val context: Context) :
+    RecyclerView.Adapter<CoinInfoAdapter.CoinInfoViewHolder>() {
+
+    var coinInfoList: List<CoinPriceInfo> = listOf()
+        set(value) {
+            field = value
+            notifyDataSetChanged()
+        }
+
+    var onCoinClickListener: OnCoinClickListener? = null
+
+    inner class CoinInfoViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val ivLogoCoin = itemView.ivLogoCoin
+        val tvSymbols = itemView.tvSymbols
+        val tvPrice = itemView.tvPrice
+        val tvLastUpdate = itemView.tvLastUpdate
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CoinInfoViewHolder {
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.item_coin_info, parent, false)
+        return CoinInfoViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: CoinInfoViewHolder, position: Int) {
+        val coin = coinInfoList[position]
+        with(holder) {
+            val symbolsTemplate = context.resources.getString(R.string.symbols_template)
+            val lastUpdateTemplate = context.resources.getString(R.string.last_update_template)
+            tvSymbols.text = String.format(symbolsTemplate, coin.fromSymbol, coin.toSymbol)
+            tvPrice.text = coin.price.toString()
+            tvLastUpdate.text = String.format(lastUpdateTemplate, coin.getFormattedTime())
+            Picasso.get().load(coin.getFullImageUrl()).into(ivLogoCoin)
+            itemView.setOnClickListener { onCoinClickListener?.onCoinClick(coin) }
+        }
+    }
+
+    override fun getItemCount() = coinInfoList.size
+
+    interface OnCoinClickListener {
+        fun onCoinClick(coinPriceInfo: CoinPriceInfo)
+    }
+}
