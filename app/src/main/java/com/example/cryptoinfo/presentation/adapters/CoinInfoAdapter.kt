a8395116e05093ecdf016c6e1a66280d16011e6b
feltsbar <kuzmin.max6569@gmail.com>
2022-07-03 23:15:39 +0300

    Added DiffUtilCallback and remove notifyDataSetChanged

--- app/src/main/java/com/example/cryptoinfo/presentation/adapters/CoinInfoAdapter.kt
+++ app/src/main/java/com/example/cryptoinfo/presentation/adapters/CoinInfoAdapter.kt
@@ -3,19 +3,16 @@ package com.example.cryptoinfo.presentation.adapters
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.ListAdapter
 import com.example.cryptoinfo.R
 import com.example.cryptoinfo.databinding.ItemCoinInfoBinding
 import com.example.cryptoinfo.domain.CoinInfo
 import com.squareup.picasso.Picasso
 
-class CoinInfoAdapter(private val context: Context) : RecyclerView.Adapter<CoinInfoViewHolder>() {
+class CoinInfoAdapter(
+    private val context: Context
+) : ListAdapter<CoinInfo, CoinInfoViewHolder>(CoinInfoDiffCallback) {
 
-    var coinInfoList: List<CoinInfo> = listOf()
-        set(value) {
-            field = value
-            notifyDataSetChanged()
-        }
     var onCoinClickListener: OnCoinClickListener? = null
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CoinInfoViewHolder {
@@ -28,7 +25,7 @@ class CoinInfoAdapter(private val context: Context) : RecyclerView.Adapter<CoinI
     }
 
     override fun onBindViewHolder(holder: CoinInfoViewHolder, position: Int) {
-        val coin = coinInfoList[position]
+        val coin = getItem(position)
         with(holder.binding) {
             val symbolsTemplate = context.resources.getString(R.string.symbols_template)
             val lastUpdateTemplate = context.resources.getString(R.string.last_update_template)
@@ -40,8 +37,6 @@ class CoinInfoAdapter(private val context: Context) : RecyclerView.Adapter<CoinI
             root.setOnClickListener { onCoinClickListener?.onCoinClick(coin) }
         }
     }
-
-    override fun getItemCount() = coinInfoList.size
 
     interface OnCoinClickListener {
         fun onCoinClick(coinPriceInfo: CoinInfo)
